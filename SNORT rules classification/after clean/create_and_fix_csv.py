606d2681b1d07596e9bbaa2c2e2dc05f66caa995
shay <shaygila@post.bgu.ac.il>
2024-03-04 01:19:42 +0200

    update

--- SNORT rules classification/after clean/create_and_fix_csv.py
+++ SNORT rules classification/after clean/create_and_fix_csv.py
@@ -10,8 +10,22 @@ df = df.drop(columns=["Unnamed: 4", "Unnamed: 5"])
 # Sort the "sid" column
 df.sort_values("SID", inplace=True)
 
-# Change the "MITRE Technique ID" column value
-df["MITRE Technique ID"] = df["MITRE Technique ID"].apply(lambda x: f"['T{int(float(x)):04d}']" if pd.notnull(x) and x.replace('.','',1).isdigit() else x)
+# # Change the "MITRE Technique ID" column value
+# df["MITRE Technique ID"] = df["MITRE Technique ID"].apply(lambda x: f"['T{int(float(x)):04d}']" if pd.notnull(x) and any(part.replace('.','',1).isdigit() for part in x.split(',')) else x)
+
+# Add square brackets to the "MITRE Technique ID" column values with commas
+# df["MITRE Technique ID"] = df["MITRE Technique ID"].apply(lambda x: f"[{', '.join([f'T{int(float(part.strip())):04d}' for part in str(x).split(',') if part.replace('.','',1).strip().isdigit()])}]" if pd.notnull(x) else x)
+
+
+# Function to extract and format values from the third column
+def extract_mitre_values(row):
+    values = row.split(",")
+    formatted_values = [f'T{int(float(part.strip())):04d}' for part in values if part.replace('.','',1).strip().isdigit()]
+    return formatted_values
+
+# Applying the function to create the new column
+df["MITRE Technique ID"] = df["MITRE Technique ID"].apply(extract_mitre_values)
+
 
 # Remove "Rule" from the "reference" column
 df["Rule"] = df["Rule"].astype(str).apply(lambda x: re.sub(r"reference:url,attack\.mitre\.org/techniques/", "", x))
